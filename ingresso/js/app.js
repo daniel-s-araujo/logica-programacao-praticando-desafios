02f6a0d051805e8f625afdb7466386fb487e0766
Daniel Araújo <daniels_araujo@outlook.com.br>
2026-09-16 19:46:24 -0300

    Realizando compra para tipo pista

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -1,6 +1,21 @@
 function comprar() {
     let tipo = document.getElementById('tipo-ingresso');
-    let qtd = document.getElementById('qtd');
-    alert(tipo.value);
-    alert(qtd.value);
+    let qtd = parseInt(document.getElementById('qtd').value);
+    
+    if (tipo.value == 'pista') {
+        comprarPista(qtd);
+    }
+}
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponível para tipo pista');
+    } else {
+        alert('Compra realizada com sucesso');
+    }
+
+    qtdPista -= qtd;
+    document.getElementById('qtd-pista').textContent = qtdPista;
 }
