923a0b433567ec743bb44fd49896f99b41ac2771
Daniel Araújo <daniels_araujo@outlook.com.br>
2026-09-16 20:56:37 -0300

    Realizando compra para outros tipos

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -4,6 +4,10 @@ function comprar() {
     
     if (tipo.value == 'pista') {
         comprarPista(qtd);
+    } else if (tipo.value == 'superior') {
+        comprarSuperior(qtd);
+    } else {
+        comprarInferior(qtd);
     }
 }
 
@@ -18,4 +22,30 @@ function comprarPista(qtd) {
 
     qtdPista -= qtd;
     document.getElementById('qtd-pista').textContent = qtdPista;
+}
+
+function comprarSuperior(qtd) {
+    let qtdSuperior = parseInt(document.getElementById('qtd-superior').textContent);
+
+    if (qtd > qtdSuperior) {
+        alert('Quantidade indisponível para tipo superior');
+    } else {
+        alert('Compra realizada com sucesso');
+    }
+
+    qtdSuperior -= qtd;
+    document.getElementById('qtd-superior').textContent = qtdSuperior;
+}
+
+function comprarInferior(qtd) {
+    let qtdInferior = parseInt(document.getElementById('qtd-inferior').textContent);
+
+    if (qtd > qtdInferior) {
+        alert('Quantidade indisponível para tipo inferior');
+    } else {
+        alert('Compra realizada com sucesso');
+    }
+
+    qtdInferior -= qtd;
+    document.getElementById('qtd-inferior').textContent = qtdInferior;
 }
