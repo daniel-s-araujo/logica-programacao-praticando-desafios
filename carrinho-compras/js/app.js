ac00a22ea21edebe1efaf6a682e3d8eba7bec930
Daniel Araújo <daniels_araujo@outlook.com.br>
2026-09-16 19:15:41 -0300

    Calculando valor total do carrinho

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,3 +1,6 @@
+let totalGeral = 0;
+limpar();
+
 function adicionar() {
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
@@ -8,11 +11,18 @@ function adicionar() {
     let carrinho = document.getElementById('lista-produtos');
     carrinho.innerHTML += `
         <section class="carrinho__produtos__produto">
-          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$${preco}</span>
+          <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R$ ${preco}</span>
         </section>
     `;
+
+    totalGeral += preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = 0;
 }
 
 function limpar() {
-
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = '';
 }
