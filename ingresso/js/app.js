080b9162509573e11ce76757856c3cca9fc4445e
Daniel Araújo <daniels_araujo@outlook.com.br>
2026-09-16 21:02:16 -0300

    Desafio: hora da prática

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -1,6 +1,11 @@
 function comprar() {
     let tipo = document.getElementById('tipo-ingresso');
     let qtd = parseInt(document.getElementById('qtd').value);
+
+    if (isNaN(qtd) || qtd <= 0) {
+        alert('Por favor, insira uma quantidade válida.');
+        return;
+    }
     
     if (tipo.value == 'pista') {
         comprarPista(qtd);
