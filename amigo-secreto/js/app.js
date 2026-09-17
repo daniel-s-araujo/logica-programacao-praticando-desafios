ffec314c352e259682d8d3b5bdc579c3a7e2cde3
Daniel Araújo <daniels_araujo@outlook.com.br>
2026-09-17 19:20:46 -0300

    Embaralhando o array de amigos

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -1,7 +1,10 @@
+let amigos = [];
+
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
     let lista = document.getElementById('lista-amigos');
-    
+    amigos.push(amigo.value);
+
     if (lista.textContent === '') {
         lista.textContent = amigo.value;
     } else {
@@ -9,4 +12,15 @@ function adicionar() {
     }
 
     amigo.value = '';    
+}
+
+function sortear() {
+    embaralha(amigos);
+}
+
+function embaralha(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
 }
